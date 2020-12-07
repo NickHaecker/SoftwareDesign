2bf8a3c214f5582f610843d27e72479958c657a7
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2020-12-07 16:35:35 +0100

    console programm

---
+++ ConsoleProgramm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
