7079dfcd2269b5f7757ab2d024f06eecdd315c9f
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2021-01-23 21:43:17 +0100

    init Softwaredesign Prüfungsthema: TranslationMemory

---
+++ TranslationMemory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TranslationMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
